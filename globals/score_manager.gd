10d81827b7f58a183d2a78a9019625b5f87ee914
Joao Vitor <jvads2005@gmail.com>
2025-12-15 22:10:20 -0300

    Add HighScoreResource and integrate high score persistence in ScoreManager

--- globals/score_manager.gd
+++ globals/score_manager.gd
@@ -1,8 +1,29 @@
 extends Node
 
 
+# .res for binary, .tres for plaintext
+const SCORES_PATH := "user://tappyscore.res"
+
+
 var high_score := 0:
 	get: return high_score
 	set(value): 
 		if value > high_score:
 			high_score = value
+			save_high_score()
+
+
+func _ready() -> void:
+	load_high_score()
+
+
+func load_high_score() -> void:
+	if ResourceLoader.exists(SCORES_PATH):
+		var hsr := load(SCORES_PATH)
+		if hsr: high_score = hsr.high_score
+
+
+func save_high_score() -> void:
+	var hsr := HighScoreResource.new()
+	hsr.high_score = high_score
+	ResourceSaver.save(hsr, SCORES_PATH)
