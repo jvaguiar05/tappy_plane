e07a7573f553dbc5125b977aa2b9c5a4067d8b8f
Joao Vitor <jvads2005@gmail.com>
2025-12-15 22:39:39 -0300

    Add SimpleChange scene and update scene transition logic in GameManager

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -5,11 +5,23 @@ extends Node
 const MAIN = preload("uid://dog4mv1bwmrsy")
 # res://scenes/game/game.tscn
 const GAME = preload("uid://bim7igp3koklm")
+# res://scenes/changes/simple_change.tscn
+const SIMPLE_CHANGE = preload("uid://b6gs0y7l2gbcj")
+
+
+var next_scene: PackedScene
+
+
+func change_to_next() -> void:
+	if next_scene:
+		get_tree().change_scene_to_packed(next_scene)
 
 
 func load_main_scene() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	next_scene = MAIN
+	get_tree().change_scene_to_packed(SIMPLE_CHANGE)
 
 
 func load_game_scene() -> void:
-	get_tree().change_scene_to_packed(GAME)
+	next_scene = GAME
+	get_tree().change_scene_to_packed(SIMPLE_CHANGE)
