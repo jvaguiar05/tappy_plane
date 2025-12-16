2aa63e6ed61843510f666ac32e6c34efe08cf632
Joao Vitor <jvads2005@gmail.com>
2025-12-15 21:30:29 -0300

    Connect laser disconnection to plane death event and refactor related functions

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -7,17 +7,30 @@ class_name Pipes
 @onready var score_sound: AudioStreamPlayer = $ScoreSound
 
 
+@onready var laser: Area2D = $Laser
+
+
 @export var speed: float = 120.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	SignalHub.on_plane_died.connect(on_plane_died)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	# Move pipes from right to left
 	position.x -= speed * delta
+
+
+func on_plane_died() -> void:
+	disconnect_laser()
+
+
+func disconnect_laser() -> void:
+	if laser.body_exited.is_connected(_on_laser_body_exited):
+		laser.body_exited.disconnect(_on_laser_body_exited)
 
 
 func _on_screen_notifier_screen_exited() -> void:
@@ -37,4 +50,5 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_laser_body_exited(body: Node2D) -> void:
 	if body is TappyPlane:
 		score_sound.play()
+		disconnect_laser()
 		SignalHub.emit_on_point_scored()
